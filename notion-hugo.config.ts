952772a6888d8e98eb1079c5651df9a3f6d2ca6a
yu Jin <27198928@qq.com>
2023-08-09 10:11:30 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://fair-land-db2.notion.site/Notion-DoIt-855cd4f17e244c6d9e618fcee42f945f?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
